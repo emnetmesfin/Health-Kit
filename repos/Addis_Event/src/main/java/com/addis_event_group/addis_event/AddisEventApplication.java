0271a19179028ccfa93409d6cdffbcd7bb1a8919
Emnet Mesfin <emnetmes2@gmail.com>
2019-01-01 01:10:46 +0300

    home page

---
+++ repos/Addis_Event/src/main/java/com/addis_event_group/addis_event/AddisEventApplication.java
@@ -0,0 +1,14 @@
+package com.addis_event_group.addis_event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddisEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddisEventApplication.class, args);
+	}
+
+}
+
