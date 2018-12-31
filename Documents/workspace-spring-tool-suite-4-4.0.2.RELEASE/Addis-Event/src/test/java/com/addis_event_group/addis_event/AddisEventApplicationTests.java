781d46e5b4c9daa0e714194c698543515ce14747
Emnet Mesfin <emnetmes2@gmail.com>
2019-01-01 00:55:04 +0300

    home page

---
+++ Documents/workspace-spring-tool-suite-4-4.0.2.RELEASE/Addis-Event/src/test/java/com/addis_event_group/addis_event/AddisEventApplicationTests.java
@@ -0,0 +1,17 @@
+package com.addis_event_group.addis_event;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AddisEventApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
